218d5c16682881180ef03fa9cad4ff6619e3bef4
Deepak Singh Suyal <148374034+deepak1suyal@users.noreply.github.com>
2024-10-09 11:53:43 +0530

    withou spzce

--- Recursion/permutation.java
+++ Recursion/permutation.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class permutation {
+    static void swap(int arr[],int a,int b){
+        int c=arr[a];
+        arr[a]=arr[b];
+        arr[b]=c;
+    }
     public static void fn(int arr[],int visited[],
-    ArrayList<Integer> l,int i){
+    ArrayList<Integer> l){
   if(l.size()==arr.length){
     System.out.println(l);
     return;
@@ -15,21 +20,36 @@ for(int k=0;k<visited.length;k++){
     if(visited[k]==0){
 l.add(arr[k]);
 visited[k]=1;
-fn(arr, visited, l, k+1);
+fn(arr, visited, l);
 visited[k]=0;
 l.remove(l.size()-1);}
 }
 //fn(arr, visited, l, i+1);
     }
+
+static void withoutspacefn(int arr[],int visited[],int i){
+
+if(i==3){for(int p=0;p<arr.length;p++)
+    System.out.print(arr[p]);
+    System.out.println();
+    return;
+}
+    for(int k=i;k<arr.length;k++){
+        swap(arr, i, k);
+        withoutspacefn(arr, visited, i+1);
+        swap(arr, i, k);
+    }
+} 
+
     public static void main(String[] args) {
         
         int arr[]={1,2,3};
         int visited[]=new int[arr.length];
         Arrays.fill(visited,0); 
         ArrayList<Integer> l=new ArrayList<>();
-        fn(arr,visited,l,0);      
+      //  fn(arr,visited,l);      
   
-  
+       withoutspacefn(arr,visited,0);
   
     }
 }
